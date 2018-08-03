6aede144f91650e037312ea8abd4ce0bfeeda18b
slowmoyang <slowmoyang@gmail.com>
2018-08-03 22:07:12 +0900

    Remove torch4hep directory. It will be a submodule

--- 1-Training/torch4hep/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from . import dataset
-from . import utils
-from . import conv_utils
--- 1-Training/torch4hep/dataset/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-from . import base
-from .pbwo4 import PbWO4Dataset
